29b2a181888b54c8f7c754965b02f4587400b033
Janee Pan <23831382@student.uwa.edu.au>
2026-03-05 13:16:45 +0800

    added branch

---
+++ fruits.py
@@ -0,0 +1,7 @@
+def favourite_fruit(fruit):
+    if fruit == 'orange':
+        return 'My favourite fruit is orange.'
+    elif fruit == 'apple':
+        return 'My favourite fruit is apple.'
+    else:
+        return 'I don\'t like it.'
